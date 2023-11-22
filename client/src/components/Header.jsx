5231e9168798fee7bc432b183c0f160f5bd9e37c
SimiyuWafulah <simiyukelvinse@gmail.com>
2023-11-22 13:51:03 +0300

    creating server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
@@ -9,7 +10,7 @@ export default function Header() {
             <span className='text-slate-500'>Q-</span>
             <span className='text-slate-700'>Estate</span>
         </h1>
-        <form>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input
                 type='search'
                 placeholder='Search...'
@@ -17,6 +18,17 @@ export default function Header() {
             />
             <FaSearch className='text-slate-700'/>
         </form>
+        <ul className='flex gap-4'>
+            <Link to='/'>
+                <li className='hidden sm:inline hover:underline text-slate-700'>Home</li>
+            </Link>
+            <Link to='/about'>
+                <li className='hidden sm:inline hover:underline text-slate-700'>About</li>
+            </Link>
+            <Link to='/signin'>
+                <li className='hover:underline text-slate-700'>Sign In</li>
+            </Link>
+        </ul>
         </div>
     </header>
   )
